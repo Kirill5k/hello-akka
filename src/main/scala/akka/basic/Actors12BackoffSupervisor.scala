3172eee0034a06a47b06cf56a0bac65402c77e97
kirill <kbibik@jaguarlandrover.com>
2019-09-16 09:20:58 +0100

    backoff supervisor

--- src/main/scala/akka/basic/Actors12BackoffSupervisor.scala
+++ src/main/scala/akka/basic/Actors12BackoffSupervisor.scala
@@ -17,13 +17,34 @@ object Actors12BackoffSupervisor extends App {
     case object ReadFile
   }
 
+  class EagerFilebasedPersistentActor extends Actor with ActorLogging {
+    private val ImportantFile = "src/main/resources/testfiles/important2.txt"
+    import FilebasedPersistentActor._
+
+    override def receive: Receive = withDataSource(Source.fromFile(new File(ImportantFile)))
+
+    def withDataSource(source: Source): Receive = {
+      case ReadFile => log.info(s"reading file ${source.getLines().toList}")
+    }
+
+    override def preStart(): Unit = {
+      log.info("eager persistent actor starting")
+    }
+
+    override def preRestart(reason: Throwable, message: Option[Any]): Unit = {
+      log.warning("eager persistent actor restarting")
+    }
+
+    override def postStop(): Unit = {
+      log.info("eager persistent actor stopped")
+    }
+  }
+
   class FilebasedPersistentActor extends Actor with ActorLogging {
     private val ImportantFile = "src/main/resources/testfiles/important2.txt"
     import FilebasedPersistentActor._
 
-    override def receive: Receive = withDataSource(null)
-
-    def withDataSource(source: Source): Receive = {
+    override def receive: Receive = {
       case ReadFile =>
         val source = Source.fromFile(new File(ImportantFile))
         log.info(s"reading file ${source.getLines().toList}")
@@ -72,6 +93,18 @@ object Actors12BackoffSupervisor extends App {
     )
   )
 
-  val stopBackoffSupervisor = system.actorOf(stopSupervisorProps, "stopSupervisor")
-  stopBackoffSupervisor ! ReadFile
+//  val stopBackoffSupervisor = system.actorOf(stopSupervisorProps, "stopSupervisor")
+//  stopBackoffSupervisor ! ReadFile
+
+  val repeatedSupervisorProps = BackoffSupervisor.props(
+    Backoff.onStop(
+      Props[EagerFilebasedPersistentActor],
+      "eagerActor",
+      3 seconds,
+      30 seconds,
+      0.2
+    )
+  )
+
+  val repeatedSupervisor = system.actorOf(repeatedSupervisorProps, "eagerSupervisor")
 }
