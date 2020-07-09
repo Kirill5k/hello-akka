47ad95425df47cc307d9fc106eeb20c88636da7f
kirill5k <immotional@aol.com>
2020-07-09 20:13:18 +0100

    word count actor

---
+++ src/main/scala/akka/actors/WordCountActor.scala
@@ -0,0 +1,20 @@
+package akka.actors
+
+import akka.actor.{Actor, Props}
+
+class WordCountActor(private val number: Int) extends Actor {
+
+  override def receive: Receive = withWordCount(0)
+
+  def withWordCount(count: Int): Receive = {
+    case text: String =>
+      println(s"""[word-counter-$number] received a message "$text"""")
+      context.become(withWordCount(count + text.split(" ").length))
+    case message =>
+      println(s"""[word-counter-$number] I cannot understand "$message"""")
+  }
+}
+
+object WordCountActor {
+  def props(number: Int) = Props(new WordCountActor(number))
+}
